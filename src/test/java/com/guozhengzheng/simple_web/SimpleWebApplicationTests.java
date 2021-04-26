02001b3ad75c0f920f409e5a9493a97949014834
guozheng.gz <guozheng.gz@cainiao.com>
2021-04-26 20:46:07 +0800

    init

---
+++ src/test/java/com/guozhengzheng/simple_web/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guozhengzheng.simple_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
